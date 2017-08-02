da877c66262e5370fbba03d245d1dc7c424a7e83
Bradik <bradnm@ya.ru>
2017-08-02 23:24:09 +0300

    partially optional  hw03

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,8 +3,17 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.BeanMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+
 public class MealTestData {
 
-    public static final BeanMatcher<Meal> MATCHER = new BeanMatcher<>();
+    public static final BeanMatcher<Meal> MATCHER = new BeanMatcher<>((expected, actual) ->
+            expected == actual ||
+            (Objects.equals(expected.getCalories(), actual.getCalories())
+                    && Objects.equals(expected.getDateTime(), actual.getDateTime())
+                    && Objects.equals(expected.getDescription(), actual.getDescription())
+                    && Objects.equals(expected.getId(), actual.getId())));
 
 }
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,79 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.DbPopulator;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+
+/**
+ * Created by Brad on 02.08.2017.
+ */
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+public class MealServiceTest {
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Autowired
+    private DbPopulator dbPopulator;
+
+    @Before
+    public void setUp() throws Exception {
+        dbPopulator.execute();
+    }
+
+
+    @Test(expected = NotFoundException.class)
+    public void get() throws Exception {
+        Meal meal = service.get(100002, 100001);
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void getBetweenDates() throws Exception {
+
+    }
+
+    @Test
+    public void getBetweenDateTimes() throws Exception {
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+}
