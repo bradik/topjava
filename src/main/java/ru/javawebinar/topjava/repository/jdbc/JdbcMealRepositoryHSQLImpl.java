3b6f65ce7d7b6a6f53b0ce29185c31cdadfa837b
Bradik <bradnm@ya.ru>
2017-08-13 17:35:02 +0300

    hw5:Optional: fixed  JdbcMealRepositoryImpl for HSQLDB from @Profile

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHSQLImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHSQLImpl.java
@@ -16,28 +16,18 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import javax.sql.DataSource;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
+@Profile("hsqldb")
 @Repository
-public abstract class JdbcMealRepositoryImpl implements MealRepository {
+public class JdbcMealRepositoryHSQLImpl extends JdbcMealRepositoryImpl {
 
-    protected static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
+    private Timestamp convertDateTime(LocalDateTime localDateTime){
+        return Timestamp.valueOf(localDateTime);
+    }
 
-    protected final JdbcTemplate jdbcTemplate;
-
-    protected final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    protected final SimpleJdbcInsert insertMeal;
-
-    @Autowired
-    public JdbcMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.insertMeal = new SimpleJdbcInsert(dataSource)
-                .withTableName("meals")
-                .usingGeneratedKeyColumns("id");
-
-        this.jdbcTemplate = jdbcTemplate;
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    public JdbcMealRepositoryHSQLImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(dataSource, jdbcTemplate, namedParameterJdbcTemplate);
     }
 
     @Override
@@ -46,7 +36,7 @@ public abstract class JdbcMealRepositoryImpl implements MealRepository {
                 .addValue("id", meal.getId())
                 .addValue("description", meal.getDescription())
                 .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
+                .addValue("date_time", convertDateTime(meal.getDateTime()))
                 .addValue("user_id", userId);
 
         if (meal.isNew()) {
@@ -67,27 +57,9 @@ public abstract class JdbcMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public boolean delete(int id, int userId) {
-        return jdbcTemplate.update("DELETE FROM meals WHERE id=? AND user_id=?", id, userId) != 0;
-    }
-
-    @Override
-    public Meal get(int id, int userId) {
-        List<Meal> meals = jdbcTemplate.query(
-                "SELECT * FROM meals WHERE id = ? AND user_id = ?", ROW_MAPPER, id, userId);
-        return DataAccessUtils.singleResult(meals);
-    }
-
-    @Override
-    public List<Meal> getAll(int userId) {
-        return jdbcTemplate.query(
-                "SELECT * FROM meals WHERE user_id=? ORDER BY date_time DESC", ROW_MAPPER, userId);
-    }
-
-    @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
         return jdbcTemplate.query(
                 "SELECT * FROM meals WHERE user_id=?  AND date_time BETWEEN  ? AND ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, startDate, endDate);
+                ROW_MAPPER, userId, convertDateTime(startDate), convertDateTime(endDate));
     }
 }
---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryPostgresImpl.java
@@ -0,0 +1,26 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Profile("postgres")
+@Repository
+public class JdbcMealRepositoryPostgresImpl extends JdbcMealRepositoryImpl {
+    public JdbcMealRepositoryPostgresImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(dataSource, jdbcTemplate, namedParameterJdbcTemplate);
+    }
+}
